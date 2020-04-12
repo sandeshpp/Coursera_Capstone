2e765ff408d397779dbb8c36a861ca5daad4df13
durga <2017durga@gmail.com>
2020-04-12 15:49:54 -0500

    added first notebook

---
+++ capstone_project.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> Data Science Capstone Project </h1>
+# <p> This is the notebook created using IBM Watson Studio. I am using it to complete my data science capstone project.</p>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course! ")
+
+
+# In[ ]:
+
+
+
+
